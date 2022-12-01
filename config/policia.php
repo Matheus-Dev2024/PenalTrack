3a2c8a39e5182e1148596eee97729a2575b3f256
Renato <renato.19gp@gmail.com>
2022-12-01 16:50:49 -0300

    Primeiro commit

---
+++ config/policia.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'nome' => env('APP_NAME', 'Zeus'), //nome do sistema
+    'codigo' => 52, //Código deste sistema na tabela de produção "sistema" no schema segurança. Não deixe como 35 no seu projeto
+    'expiracao_login' => 180, //tempo em dias para usuário perder acesso ao sistema por falta de uso
+    'registrar_log' => env('SEGURANCA_REGISTRAR_LOG', true), //habilita ou não o registro de log nos models (use apenas para teste)
+    'controle_acesso' => env('SEGURANCA_CONTROLE_ACESSO', true), //Se falso os usuários ficam com permissão total no sistema
+];
