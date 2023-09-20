2e7b447cb3af8692a191c656cae8f9fd660b2d95
Ícaro <icaromm12@gmail.com>
2023-09-20 10:08:00 -0300

    Processo de Avaliação > Cadastro > Calcular os dias de avaliação do servidor, com base no período de avaliação

--- app/Models/Facade/ProcessoAvaliacaoDB.php
+++ app/Models/Facade/ProcessoAvaliacaoDB.php
@@ -9,41 +9,40 @@ class ProcessoAvaliacaoDB
     public static function getById($processo_id)
     {
         return DB::table('processo_avaliacao as pa')
-                ->select([
-                    'pa.id as processo_id',
-                    'pa.descricao',
-                    'pa.dt_inicio_estagio as dt_inicio_estagio_en',
-                    DB::raw("TO_CHAR(pa.dt_inicio_estagio, 'DD/MM/YYYY') AS dt_inicio_estagio"),
-                    'pa.instrucao',
-                    'pa.dt_inicio_avaliacao as dt_inicio_avaliacao_en',
-                    'pa.dt_termino_avaliacao as dt_termino_avaliacao_en',
-                    DB::raw("TO_CHAR(pa.dt_inicio_avaliacao, 'DD/MM/YYYY') AS dt_inicio_avaliacao"),
-                    DB::raw("TO_CHAR(pa.dt_termino_avaliacao, 'DD/MM/YYYY') AS dt_termino_avaliacao"),
-                    DB::raw("
+            ->select([
+                'pa.id as processo_id',
+                'pa.descricao',
+                'pa.dt_inicio_estagio as dt_inicio_estagio_en',
+                DB::raw("TO_CHAR(pa.dt_inicio_estagio, 'DD/MM/YYYY') AS dt_inicio_estagio"),
+                'pa.instrucao',
+                'pa.dt_inicio_avaliacao as dt_inicio_avaliacao_en',
+                'pa.dt_termino_avaliacao as dt_termino_avaliacao_en',
+                DB::raw("TO_CHAR(pa.dt_inicio_avaliacao, 'DD/MM/YYYY') AS dt_inicio_avaliacao"),
+                DB::raw("TO_CHAR(pa.dt_termino_avaliacao, 'DD/MM/YYYY') AS dt_termino_avaliacao"),
+                DB::raw("
                         (SELECT COUNT(*) FROM processo_avaliacao_servidor pas WHERE pas.fk_processo_avaliacao = pa.id) AS qtd_servidores
                     ")
-                ])
-                ->where('pa.id', $processo_id)
-                ->first();
+            ])
+            ->where('pa.id', $processo_id)
+            ->first();
     }
-
 
 
     public static function listaProcessoAvaliacao()
     {
 
         $itensProcessoAvaliacao = DB::table('processo_avaliacao as pa')
-                                ->select([
-                                    'pa.id as id_periodo',
-                                    'pa.descricao',
-                                    DB::raw("TO_CHAR(pa.dt_inicio_avaliacao, 'DD/MM/YYYY') AS dt_inicio_avaliacao"),
-                                    DB::raw("TO_CHAR(pa.dt_termino_avaliacao, 'DD/MM/YYYY') AS dt_termino_avaliacao"),
-                                    DB::raw("
+            ->select([
+                'pa.id as id_periodo',
+                'pa.descricao',
+                DB::raw("TO_CHAR(pa.dt_inicio_avaliacao, 'DD/MM/YYYY') AS dt_inicio_avaliacao"),
+                DB::raw("TO_CHAR(pa.dt_termino_avaliacao, 'DD/MM/YYYY') AS dt_termino_avaliacao"),
+                DB::raw("
                                         (SELECT COUNT(*) FROM processo_avaliacao_servidor pas WHERE pas.fk_processo_avaliacao = pa.id) AS qtd_servidores
                                     ")
-                                ])
-                                ->whereNull('deleted_at')
-                                ->get();
+            ])
+            ->whereNull('deleted_at')
+            ->get();
 
 
         return $itensProcessoAvaliacao;
@@ -54,10 +53,9 @@ class ProcessoAvaliacaoDB
      * SRH
      */
     //public static function listarServidoresGrid($ref_inicio, $ref_termino, $dt_inicio_avaliacao, $dt_fim_avaliacao)
-    
-    
-    
-      public static function listarServidoresGrid($ref_inicio, $ref_termino)
+
+
+    public static function listarServidoresGrid($ref_inicio, $ref_termino)
     {
         $srh = config('database.connections.conexao_srh.schema');
         $policia = config('database.connections.conexao_banco_unico.schema');
@@ -72,11 +70,12 @@ class ProcessoAvaliacaoDB
                     'ss.matricula',
                     'sc.abreviacao as sigla_cargo',
                     'sc.nome as cargo',
+                    'ss.dt_admissao as data_exercicio',
                     DB::raw("TO_CHAR(ss.dt_admissao, 'DD/MM/YYYY') AS dt_admissao"),
                     'u.nome as unidade_atual'
                 )
                 ->whereBetween('ss.dt_admissao', [$ref_inicio, $ref_termino])
-                ->whereIn('ss.fk_id_cargo', [24,27,34,40])
+                ->whereIn('ss.fk_id_cargo', [24, 27, 34, 40])
                 ->where('ss.status', 1)
                 ->orderBy('ss.nome')
                 ->get();
@@ -95,11 +94,11 @@ class ProcessoAvaliacaoDB
         */
 
 
-         return $listaProcessoAvaliacao;
-     }
-    
-    
-    
+        return $listaProcessoAvaliacao;
+    }
+
+
+
     // public static function listarServidoresGrid($ref_inicio, $ref_termino)
     // {
     //     $srh = config('database.connections.conexao_srh.schema');
@@ -124,18 +123,18 @@ class ProcessoAvaliacaoDB
     //             ->orderBy('ss.nome')
     //             ->get();
 
-        /*
-        if($listaProcessoAvaliacao) {
-            foreach($listaProcessoAvaliacao as $i => $item) {
-                $info = DB::select("SELECT * FROM srh.sp_info_servidor_estagio_probatorio('$dt_inicio_avaliacao', '$dt_fim_avaliacao', $item->servidor)");
+    /*
+    if($listaProcessoAvaliacao) {
+        foreach($listaProcessoAvaliacao as $i => $item) {
+            $info = DB::select("SELECT * FROM srh.sp_info_servidor_estagio_probatorio('$dt_inicio_avaliacao', '$dt_fim_avaliacao', $item->servidor)");
 
-                if($info) {
-                    $listaProcessoAvaliacao[$i]->id_unidade_avaliacao = $info[0]->fk_id_unidade;
-                    $listaProcessoAvaliacao[$i]->unidade_avaliacao = $info[0]->lotacao;
-                }
+            if($info) {
+                $listaProcessoAvaliacao[$i]->id_unidade_avaliacao = $info[0]->fk_id_unidade;
+                $listaProcessoAvaliacao[$i]->unidade_avaliacao = $info[0]->lotacao;
             }
         }
-        */
+    }
+    */
 
 
     //     return $listaProcessoAvaliacao;
@@ -147,24 +146,23 @@ class ProcessoAvaliacaoDB
         $policia = config('database.connections.conexao_banco_unico.schema');
 
         $itensProcessoAvaliacao = DB::table('processo_avaliacao_servidor as pas')
-        ->join('processo_avaliacao as pa', 'pa.id', '=', 'pas.fk_processo_avaliacao')
-        ->join("$srh.sig_servidor as ss", 'ss.id_servidor', '=', 'pas.fk_servidor')
-        ->leftJoin("$srh.sig_documentacao_servidor as ds", 'ds.fk_servidor', 'ss.id_servidor')
-        ->leftJoin("$srh.sig_tipo_documento as td", 'ds.fk_tipo_documento' , '=', 'td.id')
-        ->join("$policia.policia.unidade as u", 'u.id', '=', 'ss.fk_id_unidade_atual')
-        ->join("$srh.sig_cargo as sc", 'sc.id', '=', 'ss.fk_id_cargo')
-        ->select(
-            'pas.id as id_processo_avaliacao',
-            'pas.fk_servidor',
-            'sc.abreviacao as sigla_cargo',
-            'pa.id',
-            'ss.nome',
-            
-            
-            
+            ->join('processo_avaliacao as pa', 'pa.id', '=', 'pas.fk_processo_avaliacao')
+            ->join("$srh.sig_servidor as ss", 'ss.id_servidor', '=', 'pas.fk_servidor')
+            ->leftJoin("$srh.sig_documentacao_servidor as ds", 'ds.fk_servidor', 'ss.id_servidor')
+            ->leftJoin("$srh.sig_tipo_documento as td", 'ds.fk_tipo_documento', '=', 'td.id')
+            ->join("$policia.policia.unidade as u", 'u.id', '=', 'ss.fk_id_unidade_atual')
+            ->join("$srh.sig_cargo as sc", 'sc.id', '=', 'ss.fk_id_cargo')
+            ->select(
+                'pas.id as id_processo_avaliacao',
+                'pas.fk_servidor',
+                'sc.abreviacao as sigla_cargo',
+                'pa.id',
+                'ss.nome',
+
+
                 DB::raw("STRING_AGG(
-                    
-                    
+
+
                     '
                     <table style=\"width: 100%;\" >
                     <tr>
@@ -177,47 +175,47 @@ class ProcessoAvaliacaoDB
                             <btn onclick=\"deletar(' || ds.id || ')\">
                                 <a href=\"#\" class=\"glyphicon glyphicon-trash\"></a>
                             </btn>
-                            
+
                         </td>
                     </tr>
                     </table>
-                    
+
                     '
-                    
+
                     ,
                   '')
                     as documentos"
-                ),    
-                
-            //  DB::raw("STRING_AGG(
-                
-            //     '<a href=\"#' || ds.id::text || '\" onclick=\"abrirPdfNovaAba(' || ds.id || ')\">
-            //         <i class=\"glyphicon glyphicon-paperclip\">&nbsp;</i>' || td.nome || '
-            //     </a>
-            //     <btn onclick=\"deletar(' || ds.id || ')\" class=\"btn\">
-            //         <i class=\"glyphicon glyphicon-trash\"></i>
-            //     </btn>
-            //     <br>',
-            //   '')
-            //     as documentos"
-            // ),
-           
-           
-             DB::raw("TO_CHAR(ss.dt_admissao, 'DD/MM/YYYY') AS dt_admissao"),'u.nome as unidade', 'ss.cargo', 'ss.matricula',)
-        ->where('pa.id' ,'=', $id_processo)
-        ->groupBy([
-            'ss.nome',
-            'pas.id',
-            'pas.fk_servidor',
-            'sc.abreviacao',
-            'pa.id',
-            'ss.dt_admissao',
-            'u.nome',
-            'ss.cargo',
-            'ss.matricula',
-            
-        ])
-        ->get();
+                ),
+
+                //  DB::raw("STRING_AGG(
+
+                //     '<a href=\"#' || ds.id::text || '\" onclick=\"abrirPdfNovaAba(' || ds.id || ')\">
+                //         <i class=\"glyphicon glyphicon-paperclip\">&nbsp;</i>' || td.nome || '
+                //     </a>
+                //     <btn onclick=\"deletar(' || ds.id || ')\" class=\"btn\">
+                //         <i class=\"glyphicon glyphicon-trash\"></i>
+                //     </btn>
+                //     <br>',
+                //   '')
+                //     as documentos"
+                // ),
+
+
+                DB::raw("TO_CHAR(ss.dt_admissao, 'DD/MM/YYYY') AS dt_admissao"), 'u.nome as unidade', 'ss.cargo', 'ss.matricula',)
+            ->where('pa.id', '=', $id_processo)
+            ->groupBy([
+                'ss.nome',
+                'pas.id',
+                'pas.fk_servidor',
+                'sc.abreviacao',
+                'pa.id',
+                'ss.dt_admissao',
+                'u.nome',
+                'ss.cargo',
+                'ss.matricula',
+
+            ])
+            ->get();
 
         return $itensProcessoAvaliacao;
     }
@@ -234,7 +232,6 @@ class ProcessoAvaliacaoDB
 
         return $servidor;
     }
-
 
 
     public static function pesquisarDescricao()
--- app/Models/Regras/ProcessoAvaliacaoRegras.php
+++ app/Models/Regras/ProcessoAvaliacaoRegras.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Regras;
 
+use App\Models\Entity\PeriodoProcessoAvaliacao;
 use App\Models\Entity\ProcessoAvaliacao;
 use App\Models\Entity\ProcessoAvaliacaoServidor;
 use App\Models\Facade\ProcessoAvaliacaoDB;
@@ -39,29 +40,37 @@ class ProcessoAvaliacaoRegras
 
         foreach ($servidoresDoProcesso as $servidor) {
             //para cada servidor, localizar a sua unidade de avaliação
-            self::salvarProcessoAvaliacaoServidorIndividual($novoProcesso, $servidor->servidor);
+            self::salvarProcessoAvaliacaoServidorIndividual($novoProcesso, $servidor->servidor, $servidor->data_exercicio);
         }
     }
 
-    public static function salvarProcessoAvaliacaoServidorIndividual(ProcessoAvaliacao $processo, int $id_servidor)
+    public static function salvarProcessoAvaliacaoServidorIndividual(ProcessoAvaliacao $processo, int $id_servidor, $data_exercicio)
     {
+        $periodoProcesso = PeriodoProcessoAvaliacao::find($processo->fk_periodo_processo);
+
         //calcular os dias de estágio com base na data início e término de avaliação
-        $data1 = new DateTime($processo->dt_inicio_avaliacao);
-        $data2 = new DateTime($processo->dt_termino_avaliacao);
-        $diferenca = $data1->diff($data2);
+        $dataInicio = new DateTime(date('Y-m-d', strtotime("+{$periodoProcesso->dia_inicial} days", strtotime($data_exercicio))));
+        $dataFinal = new DateTime(date('Y-m-d', strtotime("+{$periodoProcesso->dia_final} days", strtotime($data_exercicio))));
+
+        $dataInicioString = $dataInicio->format('d-m-y');
+        $dataFinalString = $dataFinal->format('d-m-y');
+
+        $diferenca = $dataInicio->diff($dataFinal);
         $diasDeEstagio = $diferenca->days;
 
         //pega o dia de trabalho programado (verificar se esse campo ainda é necessário)
         $diasDeTrabalhoProgramado = 180;
-
-        $info = DB::select("SELECT * FROM srh.sp_info_servidor_estagio_probatorio('$processo->dt_inicio_avaliacao', '$processo->dt_termino_avaliacao', $id_servidor)");
+        
+        //para utilizar a função 'srh.sp_info_servidor_estagio_probatorio', o formato da data precisa ser "d-m-y"
+        $info = DB::select("SELECT * FROM srh.sp_info_servidor_estagio_probatorio('$dataInicioString', '$dataFinalString', $id_servidor)");
         if ($info) {
             ProcessoAvaliacaoServidor::create([
                 'fk_processo_avaliacao' => $processo->id,
                 'fk_servidor' => $id_servidor,
                 'fk_unidade' => $info[0]->fk_id_unidade,
                 'dias_estagio' => $diasDeEstagio,
-                'dias_trabalho_programado' => $diasDeTrabalhoProgramado
+                'dias_trabalho_programado' => $diasDeTrabalhoProgramado,
+                'dias_ausencia' => $info[0]->dias_ausencia
             ]);
         }
     }
